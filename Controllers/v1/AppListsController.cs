6f9121487f0ef9731444be5e3b15d4fc5d9f3020
alexchow <cplover@live.com>
2018-05-13 01:54:27 +0800

    update

--- Controllers/v1/AppListsController.cs
+++ Controllers/v1/AppListsController.cs
@@ -8,6 +8,7 @@ using AppListWebAPI.Models;
 using BahamutCommon.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using BahamutCommon.Encryption;
 
 namespace AppListWebAPI.Controllers.v1
 {
@@ -43,106 +44,109 @@ namespace AppListWebAPI.Controllers.v1
         private static string AESEncryptPayload(object payloadModel, string secret)
         {
             var modelJson = Newtonsoft.Json.JsonConvert.SerializeObject(payloadModel);
-            var payload = BahamutCommon.Encryption.AESHelper.AESEncrypt(modelJson, secret);
+            var payload = AESHelper.AESEncrypt(modelJson, secret);
             return payload;
         }
 
-        private static T RSADecryptPayload<T>(string payload, string rsaPriKey)
+        private static T AESDecryptPayload<T>(string payloadEncrypted, string secret)
+        {
+            var modelJson = AESHelper.AESDecrypt(payloadEncrypted, secret);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(modelJson);
+        }
+
+        private static string[] RSADecryptKey(string keyEncrypted, string rsaPriKey)
         {
             using (var rsap = new RSACryptoServiceProvider())
             {
                 var cspBlob = Convert.FromBase64String(rsaPriKey);
                 rsap.ImportCspBlob(cspBlob);
-
-                var payloadB64Bytes = Convert.FromBase64String(payload);
-                var modelJsonBytes = rsap.DecryptValue(payloadB64Bytes);
-                var modelJson = System.Text.Encoding.UTF8.GetString(modelJsonBytes);
-                var payloadModel = Newtonsoft.Json.JsonConvert.DeserializeObject<T>(modelJson);
-                return payloadModel;
+                var keyBytes = rsap.Decrypt(Convert.FromBase64String(keyEncrypted), true);
+                var key = System.Text.ASCIIEncoding.ASCII.GetString(keyBytes);
+                return key.Split(":");
             }
         }
 
-        [HttpPost("{payload}")]
-        public object GetAppList(string payload)
+        [HttpPost]
+        public object GetAppList(string payload, string key)
         {
-            using (var rsap = new RSACryptoServiceProvider())
+            var keyInfo = RSADecryptKey(key, Startup.APIRequestPayloadRSAPrivateKey);
+            var algorithm = keyInfo[0];
+            var secretKey = keyInfo[1];
+            var model = AESDecryptPayload<AppListRequestModel>(payload, secretKey);
+
+            if (string.IsNullOrEmpty(model.deviceId) || string.IsNullOrEmpty(model.uniqueId) || model.ts == 0)
             {
-                var model = RSADecryptPayload<AppListRequestModel>(payload, Startup.APIRequestPayloadRSAPrivateKey);
-
-                if (string.IsNullOrEmpty(model.deviceId) || string.IsNullOrEmpty(model.uniqueId) || model.ts == 0)
-                {
-                    Response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
-                    return new ApiResult
-                    {
-                        code = Response.StatusCode,
-                        msg = "Invalid Parameters"
-                    };
-                }
-
-                if (!model.TestSignature())
-                {
-                    Response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
-                    return new ApiResult
-                    {
-                        code = Response.StatusCode,
-                        msg = "Invalid Signature"
-                    };
-                }
-
-                if (string.IsNullOrEmpty(model.channel))
-                {
-                    model.channel = BTAppLaunchRecord.CHANNEL_UNKNOW;
-                }
-
-                var a = from u in DBContext.BTAppLaunchRecord where u.DeviceId == model.deviceId && u.Platform == model.platform && u.UniqueId == model.uniqueId select u;
-                if (a.Any())
-                {
-                    var record = a.First();
-                    record.Channel = model.channel;
-                    record.BundleId = model.bundleId;
-                    record.UrlSchemes = model.urlSchemes;
-                    record.LaunchDateTs = DateTimeUtil.UnixTimeSpanSec;
-                    DBContext.BTAppLaunchRecord.Update(record);
-                }
-                else
-                {
-                    DBContext.BTAppLaunchRecord.Add(new BTAppLaunchRecord
-                    {
-                        DeviceId = model.deviceId,
-                        Platform = model.platform,
-                        UniqueId = model.uniqueId,
-                        Channel = model.channel,
-                        BundleId = model.bundleId,
-                        UrlSchemes = model.urlSchemes,
-                        LaunchDateTs = DateTimeUtil.UnixTimeSpanSec
-                    });
-                }
-                DBContext.SaveChanges();
-
-                var dateTimeLimited = DateTimeUtil.UnixTimeSpanOfDateTime(DateTime.Now.AddDays(-30)).TotalSeconds;
-
-                var resList = from u in DBContext.BTAppLaunchRecord
-                              where u.DeviceId == model.deviceId && u.Platform == model.platform && u.LaunchDateTs > dateTimeLimited
-                              select new
-                              {
-                                  UniqueId = u.UniqueId,
-                                  Channel = u.Channel,
-                                  BundleId = u.BundleId,
-                                  UrlSchemes = u.UrlSchemes,
-                                  LaunchDateTs = u.LaunchDateTs
-                              };
+                Response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
                 return new ApiResult
                 {
-                    code = 200,
-                    msg = "Success",
-                    content = AESEncryptPayload(new
-                    {
-                        DeviceId = model.deviceId,
-                        Platform = model.platform,
-                        AppList = resList
-                    }, model.aesKey)
+                    code = Response.StatusCode,
+                    msg = "Invalid Parameters"
                 };
             }
+
+            if (!model.TestSignature())
+            {
+                Response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
+                return new ApiResult
+                {
+                    code = Response.StatusCode,
+                    msg = "Invalid Signature"
+                };
+            }
+
+            if (string.IsNullOrEmpty(model.channel))
+            {
+                model.channel = BTAppLaunchRecord.CHANNEL_UNKNOW;
+            }
+
+            var a = from u in DBContext.BTAppLaunchRecord where u.DeviceId == model.deviceId && u.Platform == model.platform && u.UniqueId == model.uniqueId select u;
+            if (a.Any())
+            {
+                var record = a.First();
+                record.Channel = model.channel;
+                record.BundleId = model.bundleId;
+                record.UrlSchemes = model.urlSchemes;
+                record.LaunchDateTs = DateTimeUtil.UnixTimeSpanSec;
+                DBContext.BTAppLaunchRecord.Update(record);
+            }
+            else
+            {
+                DBContext.BTAppLaunchRecord.Add(new BTAppLaunchRecord
+                {
+                    DeviceId = model.deviceId,
+                    Platform = model.platform,
+                    UniqueId = model.uniqueId,
+                    Channel = model.channel,
+                    BundleId = model.bundleId,
+                    UrlSchemes = model.urlSchemes,
+                    LaunchDateTs = DateTimeUtil.UnixTimeSpanSec
+                });
+            }
+            DBContext.SaveChanges();
+
+            var dateTimeLimited = DateTimeUtil.UnixTimeSpanOfDateTime(DateTime.Now.AddDays(-30)).TotalSeconds;
+
+            var resList = from u in DBContext.BTAppLaunchRecord
+                          where u.DeviceId == model.deviceId && u.Platform == model.platform && u.LaunchDateTs > dateTimeLimited
+                          select new
+                          {
+                              UniqueId = u.UniqueId,
+                              Channel = u.Channel,
+                              BundleId = u.BundleId,
+                              UrlSchemes = u.UrlSchemes,
+                              LaunchDateTs = u.LaunchDateTs
+                          };
+            return new ApiResult
+            {
+                code = 200,
+                msg = "Success",
+                content = AESEncryptPayload(new
+                {
+                    DeviceId = model.deviceId,
+                    Platform = model.platform,
+                    AppList = resList
+                }, model.aesKey)
+            };
         }
     }
 }
